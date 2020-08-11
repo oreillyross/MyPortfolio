ea9415a96f95a17b91ad49311acd0ad9f01336fa
Faktor 10 <oreillyross@gmail.com>
2020-08-11 17:06:06 +0200

    add a how I work page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function IndexPage() {
   return (
     <div>
-      Hello World.{' '}
+      Hello World.{" "}
       <Link href="/about">
         <a>About</a>
       </Link>
+      <Link href="/work">
+        <a>Work</a>
+      </Link>
     </div>
-  )
+  );
 }
