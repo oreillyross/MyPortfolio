e5f2c3f4be7cf4c247e6a72300cb60c85f1b8c32
Ross O'Reilly <oreillyross@gmail.com>
2020-12-10 11:31:27 +0100

    Revert "add svg"
    This reverts commit ada0384a6875267992835c99bbf914f63bb5b5c0.

--- components/Hero.js
+++ components/Hero.js
@@ -1,11 +1,37 @@
 
 
+<<<<<<< HEAD
 export default function Hero() {
   return (
     <section className='hero'>
   <h1 className='hero-name'>Hi, My name is Ross </h1>
   <h3 className='hero-strap'>self taught frontend engineer building awesome apps using React, Typescript, and GraphQL</h3>
   <div className='hero-img' ><img width='400px' height='400px' src='img/undraw.svg'/></div>
+=======
+
+const StyledName = styled.div`
+  font-size: 1em;
+  @media (min-width: 400px) {
+    font-size: 2em;
+  }
+ `
+const StyledStrap = styled.h3`
+  color: #40A8C4;
+  font-size: 1em;
+  @media (min-width: 400px) {
+    font-size: 2em;
+  }
+`
+
+
+export default function Hero() {
+  return (
+    <section className={styles.hero}>
+  <StyledName>Ross O'Reilly</StyledName>
+  <article>
+    <StyledStrap>Full stack software developer</StyledStrap>
+  </article>
+>>>>>>> parent of ada0384... add svg
 </section>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Hero from "../components/Hero";
+<<<<<<< HEAD
 import Main from '../components/Main'
 
 
 
+=======
+import Head from "next/head";
+>>>>>>> parent of ada0384... add svg
 
 export default function IndexPage() {
   
@@ -10,9 +14,21 @@ export default function IndexPage() {
     <div>
 
     <Hero/>
+<<<<<<< HEAD
     <Main/>
 
 
+=======
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/howiwork">
+        <a>Work</a>
+      </Link>
+      After a long sabbatical I have returned to the world of software development. I tinkered with computers for as long as I can remember, starting out with Amiga BASIC, formerly studying CS, COBOL, Pascal, Delphi and working in Java in the late 90's. I left the industry in early 2000 disillusioned with the state of web applications and a yearning to see what else the world had to offer. After a 13 year long round-trip travelling the world in the field of travel security and risk management I re-enter the world of software development. Excited by how much has changed and pleased that cloud based computing is an actual thing now. 
+Absorbed by the developer culture, and actively engaged with developing apps using HTML5, CSS3, Javascript (ES5, ES6+), React, NextJs, Gatsby, GraphQL, Node.js along with the host of great supporting libraries to many to mention.
+    
+>>>>>>> parent of ada0384... add svg
     </div>
   );
 }
