8f2b23e348d925895fbe8ed11da48a975197484c
Ross O'Reilly <oreillyross@gmail.com>
2020-12-13 15:34:41 +0100

    add main page link

--- components/Main.js
+++ components/Main.js
@@ -3,7 +3,7 @@ export default function Main() {
     return (
 <>
         <div id='chevron'></div>
-        <div className='main'>Scroll</div>
+        <div className='main'>How I can help?</div>
         </>
     )
 }
