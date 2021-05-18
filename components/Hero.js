13fbfd57ff7f94763db177d6f2a79068196893eb
Faktor 10 <oreillyross@gmail.com>
2021-05-18 12:20:11 +0200

    update

--- components/Hero.js
+++ components/Hero.js
@@ -1,13 +1,12 @@
+
+
 export default function Hero() {
   return (
-    <section className="hero">
-      <h1 className="hero-name">Hi, My name is Ross </h1>
-      <h3 className="hero-strap">
-        self taught frontend engineer building awesome apps using React,
-        Typescript, and GraphQL
-      </h3>
-      <div className="hero-img"></div>
-      <div>Something else goes here</div>
-    </section>
-  );
+    <section className='hero'>
+  <h1 className='hero-name'>Hi, My name is Ross </h1>
+  <h3 className='hero-strap'>self taught frontend engineer building awesome apps using React, Typescript, and GraphQL</h3>
+  <div className='hero-img' ><img width='400px' height='400px' src='img/undraw_freelancer.svg'/></div>
+  <div>Something else goes here</div>
+</section>
+  )
 }
