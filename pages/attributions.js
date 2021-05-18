9aca11ae7edd9d704b02180e78a73d3b354b7bc7
Faktor 10 <oreillyross@gmail.com>
2021-05-18 12:19:45 +0200

    accepted changes from Github

--- pages/attributions.js
+++ pages/attributions.js
@@ -1,11 +1,10 @@
 export default function Attributions() {
-    return (
-        <div>
-            It would not have been possgit ible to create my apps without the work of these splendid people, as such a big shout out to the following:
-
-            <a href='https://www.svgbackgrounds.com/license/'>SVG Backgrounds</a>
-            <a href='https://undraw.co/license'>unDraw SVG illustrations</a>
-        </div>
-    )
+  return (
+    <div>
+      It would not have been possible to create my apps without the work of
+      these splendid people, as such a big shout out to the following:
+      <a href="https://www.svgbackgrounds.com/license/">SVG Backgrounds</a>
+      <a href="https://undraw.co/license">unDraw SVG illustrations</a>
+    </div>
+  );
 }
-
