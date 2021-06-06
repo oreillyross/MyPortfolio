e81aabeaa59d0cab75febfe0c3826ca07fd3e87d
Ross O'Reilly <oreillyross@gmail.com>
2021-06-06 17:36:43 +0200

    add all text of my story

--- pages/day/markdown/howiwork.md
+++
@@ -1,7 +0,0 @@
-I get by just fine using a Windows 10 laptop running WSL2 (ubuntu distro).
- 
-I code mostly in VS Code and am trying my hand at using the VIM mode. I adhere to the strict linting practices, using ESLint, and Prettier installed globally for auto-formatting the code with the auto format on save option to save time and energy.
-
-I enjoy using traditional SQL databases, such as postgres SQL, but I am equally comfortable using document databases, such as MongoDB. I have yet to properly explore KV stores.
-
-I try to not use starter kits, except for the basic starter kit of whichever framework or library I am using. NextJS, Gatsby, and React (create-react-app) all have pretty sensible defaults and saves me heaps of time in setting up the Typescript/Babel, ESLint, Webpack, Dev Server, Jest Testing pipeline and configuration.
--- pages/mystory.js
+++ pages/mystory.js
@@ -26,7 +26,55 @@ export default function MyStory() {
         <h3>My story</h3>
       </AboutHeader>
       <div className={imgStyle}>
-        I began programming in the early _1990s_. <br />
+        In late 2014 through a desire to re-explore the world of software
+        development I began looking at what online courses were being offered.
+        At the time I really did not know what I wanted to work on but I knew I
+        had to choose carefully knowing how quickly fads in the world of
+        software engineering come and go. Through a process of elimination I
+        arrived 4 years later at a core competency of being a full stack
+        software developer using Typescript, Javascript, Node, HTML, CSS and the
+        React library and NextJS and Gatsby frameworks. My journey began with me
+        learning Scala having taken Martin Odersky’s wonderful online course
+        through Coursera. Having coded in Java, and Delphi before I left the
+        industry in the late 90’s I felt like Scala was a natural place to
+        start. Having completed Martin’s Functional Programming course in
+        December of 2014, I looked around at more similar courses in functional
+        programming as this was an area I was not familiar with, having come
+        from a strong Object Oriented Programming (OOP) background. In the
+        following months I went on to complete similar courses led by Martin and
+        two other computer scientists Roland Kuhn and Erik Meijer. I completed
+        the course Principles of Reactive Programming in June 2015 which was
+        also being offered by Coursera at the time. And so the journey went,
+        where I dabbled in completing a raft of courses to try to get a feel
+        where I not only would excel, but also a skillset which I could grow
+        into. I completed the MongoDB for Java developers course, Java Essential
+        training, and Javascript essential training on the Lynda training site.
+        I completed the Java SE 8 MOOC course on Lambdas and Streams in July
+        2015. As my interest grew in the field of functional programming, I also
+        realised my education in Computer Studies which I took in mid 1990’s had
+        only covered Systems design and business applications of COBOL, and
+        Pascal code. I subsequently explored more fundamental computer science
+        subjects and took the online course Paradigms of Computer Programming
+        offered by Peter Van Roy from UCL, in France. I also completed a
+        follow-on course offered by Erik Meijer through Delft University, called
+        Introduction to Functional Programming. During this time I had been
+        using the general purpose language Javascript because many of the
+        functional programming techniques could easily be applied using
+        Javascript. The language began to grow on me, and I was already
+        exploring writing web apps using React, and had been closely following
+        the rapid adoption of React in the wider community. When typescript was
+        released, I was hooked after I realised Anders Hejlsberg was the core
+        architect. Anders wrote Turbo Pascal, one of the first formal languages
+        I studied and used in production. I had found the space I wanted to
+        focus on in Software engineering. This was around 2018, and I realised
+        that if I wanted to really excel in this space like all the cool kids on
+        the block, I had to properly learn HTML, and CSS. The following two
+        years I followed a number of online courses and learnt from some of the
+        best such as Erik Meyer, and Rachel Andrews, both CSS experts. And here
+        we are, in 2021. I have a strong grasp of Typescript, Javascript, NodeJS
+        development using the React library, and all its associated popular
+        libraries such as React Router and the CSS-in-JS libraries, Emotion, and
+        Styled Components. I began programming in the early _1990s_. <br />
         <br />
         <Image src="/img/back_in_the_day.svg" width={650} height={850} />
         I started with Amiga BASIC and then formally studied computer science
