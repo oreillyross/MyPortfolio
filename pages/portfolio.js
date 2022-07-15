2f0a1ec65376330d0adc27e3f0920ae61717dd97
Faktor 10 <oreillyross@gmail.com>
2022-07-15 13:03:01 +0200

    add Brainy notes

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -34,7 +34,19 @@ export default function Portfolio() {
               React Strictly Typed
               <Image src="/img/codesandbox.webp" width={20} height={20} />
             </li>
-            <li>Horizon</li>
+            <li>Brainy Notes</li>
+             <p>This project is a spin-off from the much over done Blog post, and tod list apps that everyone does when demostrating a certain piece of technology, 
+               language, framework etc. I decided to create this project because it also solves a personal pain point for me, as I am constantly saving snippets of 
+                 code, good practices, config settings etc and as of yet havent found a reasonable place to sotre this data for easy retrieval.
+                 
+                 So I created brainy notes. It is built using create react app and typescript, uses supabase as the BAAS for data storage, tailwindCSS for styling, react router, and 
+react query for caching.
+The app is intended to be re(built) multiple times over as I learn other technoligies. Linraries which I really want to try out next include:
+1. tRPC, 
+2. Prisma and Apollo
+3. NX monorepo
+  
+                 </p>
           </ul>
         </section>
       </section>
