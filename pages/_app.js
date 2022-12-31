87eac7bd21cd6433e089fcea8d6181903cd9533e
faktor10 <oreillyross@gmail.com>
2022-12-31 11:44:41 +0100

    reset

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,11 @@
 import "../style/styles.css";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
-import { css } from "@emotion/css";
 
-const app = css`
-  width: 95vw;
-  margin: 0 auto;
-`;
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <div className={app}>
+    <div>
       <div className="wrapper">
         <div>
         </div>
