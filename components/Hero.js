e46b80b46c8b7a17af2796391f47e347bf3db53e
faktor10 <oreillyross@gmail.com>
2023-03-05 16:55:39 +0100

    add

--- components/Hero.js
+++ components/Hero.js
@@ -1,12 +1,17 @@
-
-
+import {Link} from "next"
 export default function Hero() {
   return (
-    <section className='hero'>
-  <h1 className='hero-name'>Hi, My name is Ross </h1>
-  <h3 className='hero-strap'>self taught frontend engineer building awesome apps using React, Typescript, and GraphQL</h3>
-  <div className='hero-img' ><img width='400px' height='400px' src='img/undraw_freelancer.svg'/></div>
-  <div>Something else goes here</div>
-</section>
-  )
+    <section className="hero">
+      <h1 className="hero-name">Hi, My name is Ross </h1>
+      <h3 className="hero-strap">
+        self taught software engineer building awesome apps using{" "}
+        <Link href="/jj" >React</Link>, Typescript, GraphQL,
+        {/* TODO Add a whole lot more tech that you use */}
+      </h3>
+      <div className="hero-img">
+        <img width="400px" height="400px" src="img/undraw_freelancer.svg" />
+      </div>
+      <div>Something else goes here</div>
+    </section>
+  );
 }
