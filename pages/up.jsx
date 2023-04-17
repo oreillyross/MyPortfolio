536e5763a094b8052bd1862b540bbce85080de50
Faktor 10 <oreillyross@gmail.com>
2023-04-17 15:29:04 +0200

    Create up.jsx

---
+++ pages/up.jsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+      <div> Don't think. Just execute the plan. The plan is the alarm clock goes off, you get up, you go work out. Get some</div>  
+    )
+}
