11626b07ce4c6c28ea3edf5163fa7c9804a5c871
Faktor 10 <oreillyross@gmail.com>
2023-10-31 09:50:28 +0100

    Update up.jsx

--- pages/up.jsx
+++ pages/up.jsx
@@ -1,5 +1,16 @@
 export default function Page() {
     return (
       <div> Don't think. Just execute the plan. The plan is the alarm clock goes off, you get up, you go work out. Get some</div>  
+        <div>
+            <h3>Read this everytime after 2-3 drinks</h3>
+        <p>
+        Its not Really You. Its the alochol monster.
+        You are stronger than the voice. It will quickly weaken when you recognize it for what it is and put some distance between you and it.
+        Give it some space.
+        It's very noisy, I get that. But its bark is worse than its bite. It has no actual 
+        power over you. It's that little child in the backseat of the car who is desperate for an 
+        ice cream. She's not going to give in easily. BUT ultimately you're IN-CHARGE. 
+        </p>
+        </div>
     )
 }
