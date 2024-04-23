43f2cc51249be7a838e12b942e5ad9ba2c5d9ed2
Faktor 10 <oreillyross@gmail.com>
2024-04-23 14:10:29 +0000

    Reformat, simplify the front page for more impact

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
 export default function Home() {
   return (
     <>
-    <h1 className="text-green-300">Home page</h1>
-    <div>
-      This is where the home is, hopefully it loads now.
-    </div>
+      <main>
+        <aside className="m-4">
+          <h2 className="text-3xl ">Hi, </h2>
+        </aside>
+        <div className="flex justify-center items-center flex-col">
+          <img
+            src="./img/freelancer.svg"
+            width={300}
+            height={250}
+            alt="Programmer sitting at his laptop"
+          />
+          <div className="mt-4 text-2xl">
+            <h4>Software Engineer</h4>
+          </div>
+        </div>
+        <div className="p-4">
+          I am a freelance software engineer with experience coding in React,
+          TypeScript/Javascript, Python and more recently Golang.
+          <pre className="mt-2 p-2">I am a recovering Java developer</pre>
+        </div>
+        <div>
+          See my Journey<> </>
+          <button>Here...</button>
+        </div>
+      </main>
     </>
-  )
+  );
 }
