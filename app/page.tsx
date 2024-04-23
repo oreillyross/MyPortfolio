79f35465e6786a129f91be272e508c8567b4420b
Faktor 10 <oreillyross@gmail.com>
2024-04-23 13:39:55 +0000

    tailwindcss installed, ftw, 😁

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-    <h1>Home page</h1>
+    <h1 className="text-green-300">Home page</h1>
     <div>
       This is where the home is, hopefully it loads now.
     </div>
